3f86042c31c192191e8e84414385c65374847245
N4107 <kanghb@sk.com>
2025-01-20 17:00:51 +0900

    singleton container

--- core/src/test/java/hello/core/xml/XmlAppContextTest.java
+++ core/src/test/java/hello/core/xml/XmlAppContextTest.java
@@ -3,10 +3,9 @@ package hello.core.xml;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class XmlAppContext {
+public class XmlAppContextTest {
 
     @Test
     void xmlAppContext() {
