0e631b56aa0cd2361eb97c045174309931ab4ab9
CrazyAtom <crazyatom84@gmail.com>
2025-01-22 05:35:52 +0900

    lombok library 추가

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,25 +1,17 @@
 package hello.core.order;
 
 import hello.core.discount.DiscoutPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscoutPolicy discoutPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscoutPolicy discoutPolicy) {
-        this.memberRepository = memberRepository;
-        this.discoutPolicy = discoutPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -27,9 +19,5 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discoutPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
-
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
     }
 }
