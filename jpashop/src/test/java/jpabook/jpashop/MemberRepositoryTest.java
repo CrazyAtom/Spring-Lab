1844db67f9563fa78d2620c5c0ad195341aafffc
N4107 <kanghb@sk.com>
2025-02-04 10:22:28 +0900

    yml 수정, junit4 마이그레이션

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -2,13 +2,14 @@ package jpabook.jpashop;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MemberRepositoryTest {
 
@@ -17,6 +18,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception {
         // given
         Member member = new Member();
