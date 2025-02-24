6c1b22f0c95285b4941d3e64aa745a7c0c4758c4
CrazyAtom <crazyatom84@gmail.com>
2025-02-24 21:37:19 +0900

    code conventions 적용

--- servlet/src/main/java/lab/servlet/basic/request/RequestBodyJsonServlet.java
+++ servlet/src/main/java/lab/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,6 +1,12 @@
 package lab.servlet.basic.request;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.util.StreamUtils;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,29 +14,26 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lab.servlet.basic.HelloData;
-import org.springframework.util.StreamUtils;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+	private ObjectMapper objectMapper = new ObjectMapper();
 
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws
+		ServletException,
+		IOException {
+		ServletInputStream inputStream = request.getInputStream();
+		String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
-    @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletInputStream inputStream = request.getInputStream();
-        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+		System.out.println("messageBody = " + messageBody);
 
-        System.out.println("messageBody = " + messageBody);
+		HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
 
-        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+		System.out.println("helloData.username = " + helloData.getUsername());
+		System.out.println("helloData.age = " + helloData.getAge());
 
-        System.out.println("helloData.username = " + helloData.getUsername());
-        System.out.println("helloData.age = " + helloData.getAge());
-
-        System.out.println("ok");
-    }
+		System.out.println("ok");
+	}
 }
