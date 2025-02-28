bb45ce95f7d01ce38e3c9855ea3df0cb0d44af46
CrazyAtom <crazyatom84@gmail.com>
2025-02-28 13:39:39 +0900

    요청에 대해 Http method 제한 적용

--- servlet/src/main/java/lab/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/lab/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,12 +18,12 @@ public class SpringMemberControllerV3 {
 
 	private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-	@RequestMapping("/new-form")
+	@GetMapping("/new-form")
 	public String newForm() {
 		return "new-form";
 	}
 
-	@RequestMapping("/save")
+	@PostMapping("/save")
 	public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model) {
 		Member member = new Member(username, age);
 		memberRepository.save(member);
@@ -30,7 +32,7 @@ public class SpringMemberControllerV3 {
 		return "save-result";
 	}
 
-	@RequestMapping()
+	@GetMapping()
 	public String members(Model model) {
 		List<Member> members = memberRepository.findAll();
 
