d78e7687cc00321b685d242c98cbe1b028e643bf
CrazyAtom <crazyatom84@gmail.com>
2025-03-01 19:02:23 +0900

    아이템 컨트롤러 클래스 추가 - 아이템 목록, 아이템 상세 처리

---
+++ item-service/src/main/java/lab/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,45 @@
+package lab.itemservice.web.basic;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.annotation.PostConstruct;
+import lab.itemservice.domain.item.Item;
+import lab.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+	private final ItemRepository itemRepository;
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+		return "basic/items";
+	}
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable("itemId") Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "basic/item";
+	}
+
+	/**
+	 * 테스트용 상품 초기 데이터
+	 */
+	@PostConstruct
+	public void init() {
+		itemRepository.save(new Item("itemA", 10000, 10));
+		itemRepository.save(new Item("itemB", 20000, 20));
+	}
+}
