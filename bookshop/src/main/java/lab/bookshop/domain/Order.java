8d74728764473dc52f8658ee28075fe7d5e1db10
CrazyAtom <crazyatom84@gmail.com>
2025-03-04 01:38:26 +0900

    변경 사항 전파를 위해 CascadeType.ALL 적용

--- bookshop/src/main/java/lab/bookshop/domain/Order.java
+++ bookshop/src/main/java/lab/bookshop/domain/Order.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -35,10 +36,10 @@ public class Order {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne(fetch = LAZY)
+	@OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
