163e330049058a91f420530a8ae4ed02ac06a679
N4107 <kanghb@sk.com>
2025-03-06 13:14:55 +0900

    @NoArgsConstructor로 protected 생성자

--- bookshop/src/main/java/lab/bookshop/domain/OrderItem.java
+++ bookshop/src/main/java/lab/bookshop/domain/OrderItem.java
@@ -9,12 +9,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lab.bookshop.domain.Item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
 	@Id
